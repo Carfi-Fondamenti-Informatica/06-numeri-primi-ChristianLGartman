96ad18732388a81841176e5eaf4aa00b9b9b8482
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-11-30 17:59:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+int main() {
+    int h;
+    cout << "dimmi h" << endl;
+    cin >> h;
+    int a = numeroprimo(h);
+    if (w == true) {
+        cout << "numero primo" << endl;
+    }
 }
