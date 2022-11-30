e06ed6a7204ec8d4b59eba5ce22a42d12dcbbef6
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-11-30 17:57:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include "lib.h"
+int numeroprimo (int n) {
+    int i=1, x=0;
+    if (n>1) {
+        do {
+            i++;
+            x = n%i;
+        }
+        while (x!=0);
+        if (n == i) {
+            w = true;
+        }
+        else {
+            w = false;
+        }
+    }
+}
